2e7100d5376915117542ce0ba18cff9505246b97
@johnya0 <anoopjohny2000@gmail.com>
2023-08-18 11:21:27 +0200

    commit

--- ingest.py
+++ ingest.py
@@ -9,6 +9,7 @@ persist_directory = "db"
 
 def main():
     for root, dirs, files in os.walk("docs"):
+
         for file in files:
             if file.endswith(".pdf"):
                             print(file)
