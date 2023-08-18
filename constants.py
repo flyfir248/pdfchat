ae124d2d017f2565a96de1c4c7d22d3faedf8992
@johnya0 <anoopjohny2000@gmail.com>
2023-08-18 11:21:35 +0200

    commit

--- constants.py
+++ constants.py
@@ -4,6 +4,7 @@ from chromadb.config import Settings
 #Define the chroma settings
 CHROMA_SETTINGS = Settings(
     chroma_db_impl = 'duckdb+parquet',
+
     persist_directory = "db",
     anonymized_telemetry = False
 )
